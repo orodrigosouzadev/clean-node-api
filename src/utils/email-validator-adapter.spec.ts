e6354dc9b2fabc67b1c59419465ba155a4baa175
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2022-10-11 08:53:57 -0300

    refactor: rename tests

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -12,20 +12,20 @@ const makeSut = (): EmailValidatorAdapter => {
 }
 
 describe('EmailValidator Adapter', () => {
-  it('should return false if validator returns false', () => {
+  test('should return false if validator returns false', () => {
     const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
   })
 
-  it('should return true if validator returns true', () => {
+  test('should return true if validator returns true', () => {
     const sut = makeSut()
     const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
   })
 
-  it('should call validator with correct email', () => {
+  test('should call validator with correct email', () => {
     const sut = makeSut()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
     sut.isValid('any_email@mail.com')
