21437eeb866dbfdd58a7ae6227ce99b8cc15b413
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2022-10-11 08:25:41 -0300

    chore: update file extension and export

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
+  coverageDirectory: 'coverage',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**'],
-  coverageDirectory: 'coverage',
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
-}
