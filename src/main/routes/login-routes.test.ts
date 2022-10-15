84961845963d41c6d7d31876993f1aeb40ff5b5a
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2022-10-15 17:49:11 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('should return 401 on login unauthorized', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'rodrigo@rodrigosouza.dev',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
