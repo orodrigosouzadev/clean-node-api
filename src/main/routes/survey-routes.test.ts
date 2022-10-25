c07dfc911f42642aa6ab258d560edeef75549845
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2022-10-25 10:06:21 -0300

    test: ensure add survey route returns 403 if no accessToken is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -20,7 +20,7 @@ describe('Survey Routes', () => {
   })
 
   describe('POST /surveys', () => {
-    test('should return 204 on add survey success', async () => {
+    test('should return 403 on add survey without accessToken', async () => {
       await request(app)
         .post('/api/surveys')
         .send({
@@ -32,7 +32,7 @@ describe('Survey Routes', () => {
             answer: 'Answer 2'
           }]
         })
-        .expect(204)
+        .expect(403)
     })
   })
 })
