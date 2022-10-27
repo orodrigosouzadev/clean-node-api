6a24ec34e08e753de187013d9877bea5ef1ee481
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2022-10-27 19:50:49 -0300

    refactor: change models to use type alias instead of interfaces

--- src/main/decorators/log-controller-decorator.spec.ts
+++ src/main/decorators/log-controller-decorator.spec.ts
@@ -47,7 +47,7 @@ const makeFakeServerError = (): HttpResponse => {
   fakeError.stack = 'any-stack'
   return serverError(fakeError)
 }
-interface SutTypes {
+type SutTypes = {
   sut: LogControllerDecorator
   controllerStub: Controller
   logErrorRepositoryStub: LogErrorRepository
