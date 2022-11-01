689284bb738d6519e483ff89580cffc6cfb1a638
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2022-11-01 17:36:13 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Rodrigo Souza DEV - Clean Node API',
+    description: 'API desenvolvida no curso do Mango para realizar enquetes entre programadores',
+    version: '1.0.0'
+  }
+}
