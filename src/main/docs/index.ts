787da8eb19a30031361eea4c8b488bc49bd42a01
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2022-11-03 09:57:25 -0300

    docs: add login API docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,25 @@
+import { loginPath } from './paths/login-path'
+import { accountSchema } from './schemas/account-schema'
+import { loginParamsSchema } from './schemas/login-params-schema copy'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Rodrigo Souza DEV - Clean Node API',
     description: 'API desenvolvida no curso do Mango para realizar enquetes entre programadores',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
