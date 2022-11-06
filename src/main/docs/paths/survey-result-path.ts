ad96014a92bb7377c43c9b51bc989194eacda49e
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2022-11-06 17:25:25 -0300

    docs: add load survey result api docs

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -8,6 +8,7 @@ export const surveyResultPath = {
     parameters: [{
       in: 'path',
       name: 'surveyId',
+      description: 'ID da enquete a ser respondida',
       required: true,
       schema: {
         type: 'string'
@@ -22,6 +23,43 @@ export const surveyResultPath = {
         }
       }
     },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Enquete'],
+    summary: 'API para consultar o resultado de uma enquete',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      description: 'ID da enquete a ser consultada',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
     responses: {
       200: {
         description: 'Sucesso',
