17e2720bccf33334e6bb8e32b4c69d4bd8abed46
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2022-11-09 20:10:59 -0300

    refactor: add types to AddSurvey usecase

--- src/domain/usecases/add-survey.ts
+++ src/domain/usecases/add-survey.ts
@@ -1,7 +1,17 @@
-import { SurveyModel } from '@/domain/models'
-
-export type AddSurveyParams = Omit<SurveyModel, 'id'>
-
 export interface AddSurvey {
-  add (data: AddSurveyParams): Promise<void>
+  add (data: AddSurvey.Params): Promise<void>
+}
+
+export namespace AddSurvey {
+  type SurveyAnswerModel = {
+    image?: string
+    answer: string
+  }
+
+  export type Params = {
+    question: string
+    answers: SurveyAnswerModel[]
+    date: Date
+    didAnswer?: boolean
+  }
 }
--- src/infra/db/mongodb/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey-mongo-repository.ts
@@ -1,11 +1,10 @@
 import { AddSurveyRepository, LoadSurveyByIdRepository, LoadSurveysRepository } from '@/data/protocols/db'
 import { SurveyModel } from '@/domain/models'
-import { AddSurveyParams } from '@/domain/usecases'
 import { MongoHelper, QueryBuilder } from '@/infra/db'
 import { ObjectId } from 'mongodb'
 
 export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyByIdRepository {
-  async add (data: AddSurveyParams): Promise<void> {
+  async add (data: AddSurveyRepository.Params): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(data)
   }
