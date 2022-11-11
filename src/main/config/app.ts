a69d9d318af1d03f4889a2e31dfe2ab3fdbd39d1
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2022-11-11 09:46:49 -0300

    feat: setup apollo server

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
-import setupSwagger from './config-swagger'
+import setupApolloServer from './apollo-server'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
 import setupStaticFiles from './static-files'
+import setupSwagger from './swagger'
 
 const app = express()
+setupApolloServer(app)
 setupStaticFiles(app)
 setupSwagger(app)
 setupMiddlewares(app)
--- src/main/config/swagger.ts
+++ src/main/config/swagger.ts
@@ -1,7 +1,7 @@
 import { Express } from 'express'
 import { serve, setup } from 'swagger-ui-express'
 import swaggerConfig from '@/main/docs'
-import { noCache } from '../middlewares/no-cache'
+import { noCache } from '@/main/middlewares/no-cache'
 
 export default (app: Express): void => {
   app.use('/api-docs', noCache, serve, setup(swaggerConfig))
